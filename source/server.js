e9dd03aa52234339920f406ac2f02606b264301e
LaJBel Ms <71136486+lajbel@users.noreply.github.com>
2021-10-30 14:32:34 +0000

    prettier, add favicon and mobile view

--- source/server.js
+++ source/server.js
@@ -10,15 +10,15 @@ app.set('view engine', 'ejs');
 // Routes
 
 app.get('/', (req, res) => {
-    res.render(path.join(__dirname, '/site/home.html'));
+    res.render(path.join(__dirname, '/site/home.html'), {title: 'Home'});
 });
 
 app.get('/portfolio', (req, res) => {
-    res.render(path.join(__dirname, '/site/portfolio.html'));
+    res.render(path.join(__dirname, '/site/portfolio.html'), {title: 'Portfolio'});
 });
 
 app.get('/contact', (req, res) => {
-    res.render(path.join(__dirname, '/site/contact.html'));
+    res.render(path.join(__dirname, '/site/contact.html'), {title: 'Contact'});
 });
 
 app.get('/:var(kofi|donate)', (req, res) => {
