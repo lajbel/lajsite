95dde771be1e2aa68cef14759e4073453880ea29
LaJBel Ms <71136486+lajbel@users.noreply.github.com>
2021-11-03 19:20:21 +0000

    first sass

--- source/server.js
+++ source/server.js
@@ -1,10 +1,10 @@
 const express = require('express');
+const sass = require('node-sass-middleware');
 const path = require('path');
 
 const app = express();
 
 app.engine('html', require('ejs').renderFile);
-
 app.set('view engine', 'ejs');
 
 // Routes
