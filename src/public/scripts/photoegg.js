5ae98c2edd4e5366b1ec56600e1b7682991d9872
Dani Báez <71136486+lajbel@users.noreply.github.com>
2021-11-27 01:31:11 +0000

    more easter egg

--- src/public/scripts/photoegg.js
+++ src/public/scripts/photoegg.js
@@ -6,10 +6,13 @@ const funnyThings = [
 	`<a href="https://replit.com/mark">oh hi</a>`,
 	`Try with arcade in <a href="https://majorariatto.itch.io/shipped">Shipped</a>`,
 	`Yeah i'm here`,
-	`Sorry, but Minecraft Bedrock is better.`
+	`Sorry, but Minecraft Bedrock is better.`,
+	`I'm a girl`,
+	`I'm a boy`,
+	`Only for Newgrounds`
 ];
 
-easteregg.innerHTML = `"` + funnyThings[Math.floor(Math.random() * funnyThings.length)] + `"`;
+easteregg.innerHTML = `<span class="big">“</span>` + funnyThings[Math.floor(Math.random() * funnyThings.length)] + `<span class="big">”</span>`;
 
 function photoEgg() {
 	if(!easteregg.classList.contains("show")) easteregg.classList.add("show");
